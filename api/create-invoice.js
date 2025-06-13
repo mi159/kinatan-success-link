b59fef68580420199cb7990b8c474bdc3fed2886
mi159 <koko.mizu.0407@gmail.com>
2025-06-14 00:59:03 +0900

    Update create-invoice.js

--- api/create-invoice.js
+++ api/create-invoice.js
@@ -36,3 +36,4 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: 'Server error', details: err.message });
   }
 }
+export const config = { api: { bodyParser: true } };
